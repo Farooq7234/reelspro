4746bf6acc43c029fa0f47c4ffcb47e51324dec3
Umar Farooq <ufservices465@gmail.com>
2025-02-02 21:52:24 +0530

    test: Registered User

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,35 @@
+import User from "@/app/models/User";
+import { NextResponse, NextRequest } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and Password is required!" },
+        { status: 400 }
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Email already exists!" },
+        { status: 400 }
+      );
+    }
+    await User.create({ email, password });
+
+    return NextResponse.json(
+      { message: "User Registered Successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to Register!" }, { status: 500 });
+  }
+}
