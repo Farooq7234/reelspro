a528069988c46232778c9e504e85116678e8f3ce
Umar Farooq <ufservices465@gmail.com>
2025-02-04 14:59:40 +0530

    feat: Add NextAuth integration with credentials provider for authentication

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -30,6 +30,7 @@ export async function POST(req: NextRequest) {
       { status: 201 }
     );
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ error: "Failed to Register!" }, { status: 500 });
   }
 }
---
+++ lib/auth.ts
@@ -0,0 +1,66 @@
+import { NextAuthOptions } from "next-auth";
+import { connectToDatabase } from "./db";
+import User from "@/app/models/User";
+import bcrypt from "bcryptjs";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing credentials");
+        }
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("Invalid credentials");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isValid) {
+            throw new Error("Invalid credentials");
+          }
+
+          return { id: user._id.toString(), email: user.email };
+        } catch (error) {
+          throw error;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
